b2022a74f3bc1561b6c96767c7651d431e0ef195
Lior1254 <liorshemtov2008@gmail.com>
2025-03-03 13:04:22 +0200

    xml and add read and write functions

--- app/src/main/java/com/example/internal_files/MainActivity.java
+++ app/src/main/java/com/example/internal_files/MainActivity.java
@@ -4,20 +4,72 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.EditText;
+import android.widget.TextView;
 
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 
-public class MainActivity extends AppCompatActivity {
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+public class MainActivity extends AppCompatActivity
+{
+    TextView tv1;
+    EditText eD1;
+    private final String FILENAME = "Lior.txt";
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        weddings();
+        try
+        {
+            read();
+        } catch (IOException e)
+        {
+            throw new RuntimeException(e);
+        }
+
+
+    }
+
+    public void weddings()
+    {
+        tv1 = (TextView) findViewById(R.id.tv1);
+        eD1 = (EditText) findViewById(R.id.eD1);
+    }
+
+    public void read() throws IOException {
+        FileInputStream fIS= openFileInput(FILENAME);
+        InputStreamReader iSR = new InputStreamReader(fIS);
+        BufferedReader bR = new BufferedReader(iSR);
+        StringBuilder sB = new StringBuilder();
+        String line = bR.readLine();
+        while (line != null) {
+            sB.append(line+'\n');
+            line = bR.readLine();
+        }
+        bR.close();
+        iSR.close();
+        fIS.close();
+        tv1.setText(sB.toString());
+    }
+    public void write() throws IOException {
+        FileOutputStream fOS = openFileOutput(FILENAME,MODE_PRIVATE);
+        OutputStreamWriter oSW = new OutputStreamWriter(fOS);
+        BufferedWriter bW = new BufferedWriter(oSW);
+        bW.write("ss");
+        bW.close();
+        oSW.close();
+        fOS.close();
     }
 
     @Override
